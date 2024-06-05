c624ab356149f101de0119bd6a71f40a86a6166e
Cem C. Tutum <cem.celal@gmail.com>
2024-06-05 17:13:34 -0500

    clean implementation using sorted

--- lists_01.py
+++ lists_01.py
@@ -3,27 +3,30 @@
 # O(n) time complexity, O(1) space complexity
 # Example: [1, 2, 6, 7, 9, 3, 4, 10, 8] is given and 5 is missing
 
-a = [10, 8, 1, 2, 6, 7, 9, 3, 4, 1, 2]
+# a = [10, 8, 1, 2, 6, 7, 9, 3, 4, 1, 2]
 
-#%% Generate a list of random integers
 import random
 
-n = 10
-def randomIntegerList(n):
+def generate_random_integer_list(n):
+    # Generate a shuffled list of random integers (1 to n)
     a = list(range(1, n))
     random.shuffle(a)
     missing = a[0]
     return a[1:], missing
 
-print(f"Given list: {randomIntegerList(n)[0]}, missing {randomIntegerList(n)[1]}")
-
-#%% If we are allowed to use sorted() function
-
-def find_missing(x):
+def find_missing_int(x):
+    # Find the missing integer in a given list (sorted() is used)
     y = sorted(x)
     for i in range(len(y)-1):
         if y[i+1] - y[i] > 1:
             return y[i]+1
-        
-print(f"Missing integer is {find_missing(a)}")
+
+
+if __name__ == "__main__":
+    
+    n = 10
+    given_list, missing = generate_random_integer_list(n)
+    print(given_list, missing)
+    print(find_missing_int(given_list))
+    
     
