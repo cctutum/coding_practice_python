68c8e561bc1914d1cf8180883108b55e1e63044d
Cem C. Tutum <cem.celal@gmail.com>
2024-06-08 23:10:37 -0500

    added few more exercises

--- bite-size-exercises.py
+++ bite-size-exercises.py
@@ -4,5 +4,45 @@ def ReverseString(string):
 
 print(ReverseString("Apple"))
 
+#%%
 
 # Find the longest word in a string and return its length
+def FindLongestWord(string):
+    words = string.split()
+    longest = max(words, key=len)
+    return len(longest)
+
+print(FindLongestWord("Find the longest word in a string and return its length"))
+
+#%%
+
+# Replace vowels with numbers based on their position in the alphabet
+def Replace_Vowels_With_Numbers(string):
+    vowels = ['a', 'e', 'o', 'u', 'i']
+    new_string = []
+    for char in string:
+        if char.lower() in vowels:
+            position = ord(char.lower()) - ord('a') + 1
+            new_string.append(str(position))
+        else:
+            new_string.append(char)
+    return "".join(new_string)
+    
+print(Replace_Vowels_With_Numbers("Replace vowels with numbers based on their position in the alphabet"))
+
+#%%
+
+# Determine if a string has the same number of 'x' and 'o' characters
+
+def compare_Numbers(string):
+    xs, os = 0, 0
+    for char in string:
+        if char.lower() == 'x':
+            xs += 1
+        elif char.lower() == 'o':
+            os += 1
+    return xs, os
+
+print(compare_Numbers("RXxloxe vOwXls with nuxXxrs bxsOd ox thoir position in the olohxOxOX"))
+
+#%%
