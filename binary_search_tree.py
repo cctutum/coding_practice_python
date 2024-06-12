3a69b33d31b2bfc4d96fa1fc584d33045145cf6b
Cem C. Tutum <cem.celal@gmail.com>
2024-06-11 23:26:35 -0500

    added binary search tree

---
+++ binary_search_tree.py
@@ -0,0 +1,8 @@
+"""
+Given a sorted array with unique integer elements, write a function to create
+a binary search tree with minimal height.
+
+binary tree - a tree in which each node has at MOST 2 children
+binary search tree - a binary tree in which all the left children of a node
+<= n < all right descendents
+"""
